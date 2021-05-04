ae3e1a295cd76002f5d679ffaa4982516daf6277
lukaszbrzoska1 <70590554+lukaszbrzoska1@users.noreply.github.com>
2021-05-04 21:15:22 +0200

    update menu-items to navigate withRouter HOC

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -14,28 +14,33 @@ class Directory extends React.Component {
           title: 'hats',
           imageURL: '/img/hat_1920.jpg',
           id: 1,
+          linkURL: 'hats',
         },
         {
           title: 'jackets',
           imageURL: '/img/jackets_1920.jpg',
           id: 2,
+          linkURL: '',
         },
         {
           title: 'sneakers',
           imageURL: '/img/sneakers_1920.jpg',
           id: 3,
+          linkURL: '',
         },
         {
           title: 'womens',
           imageURL: '/img/womens_1920.jpg',
           size: 'large',
           id: 4,
+          linkURL: '',
         },
         {
           title: 'mans',
           imageURL: '/img/mens_1920.jpg',
           size: 'large',
           id: 5,
+          linkURL: '',
         },
       ],
     };
@@ -44,8 +49,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageURL, id, size }) => (
-          <MenuItem key={id} title={title} imageURL={imageURL} size={size} />
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageURL, size }) => (
-  <div className={`menu-item ${size ? 'size' : null}`}>
+const MenuItem = ({ title, imageURL, size, history, linkURL, match }) => (
+  <div
+    className={`menu-item ${size ? 'size' : null}`}
+    onClick={() => history.push(`${match.url}${linkURL}`)}
+  >
     <div
       className="background-image"
       style={{
@@ -17,4 +21,4 @@ const MenuItem = ({ title, imageURL, size }) => (
   </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
